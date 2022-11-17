f14494a8245673f495319c37321601ca6e3013a9
abayol <alexia.bayol@etud.univ-jfc.fr>
2022-11-17 09:19:22 +0100

    Deuxième commit Alexia

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -21,10 +21,13 @@ public class RepetitiveEvent extends Event {
      * <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
      * </UL>
      */
+    protected ChronoUnit frequency;
+    protected ArrayList<LocalDate> exceptionsDates = new ArrayList<LocalDate>();
+
+
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        this.frequency = frenquency;
     }
 
     /**
@@ -33,8 +36,7 @@ public class RepetitiveEvent extends Event {
      * @param date the event will not occur at this date
      */
     public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        datesException.add(date);
     }
 
     /**
@@ -42,8 +44,7 @@ public class RepetitiveEvent extends Event {
      * @return the type of repetition
      */
     public ChronoUnit getFrequency() {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");    
+        return exceptionsDates;   
     }
 
 }
